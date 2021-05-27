7a59c46198b3ab5ecf944e2e22ea65263ed8f3d9
Odette Quiroz <odette.qmon@gmail.com>
2021-05-26 21:05:31 -0500

    models and controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+
+puts "Cleaning users..."
+User.destroy_all if Rails.env.development?
+
+puts "Creating users..."
+User.create!(
+  email: "odette@mail.com",
+  password: "123456",
+  first_name: "Odette",
+  last_name: "Quiroz"
+)
+
+puts "Seeding finished!"
